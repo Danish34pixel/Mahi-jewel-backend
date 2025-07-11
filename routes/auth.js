789055720c966727151088b26053dd1120e2f423
Danish34pixel <danishkhaannn34@gmail.com>
2025-07-11 15:43:08 +0530

    Initiallyy

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,12 @@
+// Get all users (admin/debug)
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, "-password");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
