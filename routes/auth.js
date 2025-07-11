c0b15100897e78c131c55c64e720f924fe0ab405
Danish34pixel <danishkhaannn34@gmail.com>
2025-07-11 15:46:38 +0530

    Initiallyyy

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,3 @@
-// Get all users (admin/debug)
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({}, "-password");
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
@@ -56,7 +47,15 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-
+// Get all users (admin/debug)
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, "-password");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 // Signup route
 router.post("/signup", async (req, res) => {
   try {
