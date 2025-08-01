db3308fafa395bd30c85a4dc72e66b0d77b1a31e
Danish34pixel <danishkhaannn34@gmail.com>
2025-08-01 14:31:14 +0530

    Added missing product routes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.js");
-const productRoutes = require("./routes/product.js");
 const cartRoutes = require("./routes/cart.js");
 
 const orderRoutes = require("./routes/order.js");
@@ -26,7 +25,6 @@ app.use("/uploads", express.static(require("path").join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 
 app.use("/api/orders", orderRoutes);
