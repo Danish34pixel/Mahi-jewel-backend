eb803a65ec96946350ccf3de4e5c3bb637523db7
Danish34pixel <danishkhaannn34@gmail.com>
2025-08-01 14:19:38 +0530

    Initial commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.js");
+const productRoutes = require("./routes/product.js");
+const cartRoutes = require("./routes/cart.js");
+
+const orderRoutes = require("./routes/order.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mahi-jewel-frontend.vercel.app"],
     credentials: true,
   })
 );
@@ -22,6 +26,10 @@ app.use("/uploads", express.static(require("path").join(__dirname, "uploads")));
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
+app.use("/api/orders", orderRoutes);
 
 // MongoDB connection
 mongoose
